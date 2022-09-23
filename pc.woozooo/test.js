daca9bd4bb4a8e34bb3fc0feba278cb97dc877e7
alanhays <9980153+alanhays@user.noreply.gitee.com>
2022-09-23 15:56:26 +0800

    测试文件

--- pc.woozooo/test.js
+++ pc.woozooo/test.js
@@ -3,12 +3,12 @@ function getOaFaLa(Oa) {
     let za = Oa >> 5;
     Fa = 31 & za;
     La = 31 & za >> 5;
-    return [31 & Oa, Fa, La]
+    return [31 & Oa, Fa, La];
 }
 var getOaValue = function (Oa, Fa, La) {
     let za = (La << 5) + Fa;
-    Oa = (za << 5) + Oa
-    return Oa
+    Oa = (za << 5) + Oa;
+    return Oa;
 }
 var a = getOaFaLa(Oa)
 console.log(a)
