c092aa9afad8a8f950db7cf83e0993c9ab72615f
alanhays <9980153+alanhays@user.noreply.gitee.com>
2022-09-28 17:41:33 +0800

    唯一节点合并

--- pc.woozooo/pc.woozooo_decodeAst.js
+++ pc.woozooo/pc.woozooo_decodeAst.js
@@ -9,8 +9,8 @@ const template = require("@babel/template").default;
 global.t = types;
 
 //读取文件
-let encode_file = "G:\\hutao\\ast_code\\ast-code\\hutao\\encode.js",
-    decode_file = "G:\\hutao\\ast_code\\ast-code\\hutao\\decode_result.js";
+let encode_file = "./encode.js",
+    decode_file = "./ecode_result.js";
 
 if (process.argv.length > 2) {
     encode_file = process.argv[2];
@@ -108,6 +108,118 @@ const decodeSwitchNesting = {
 }
 
 traverse(ast, decodeSwitchNesting);
+
+
+function getItemFromTestValue(path, number, is_remove) {
+    let {cases} = path.node;
+    let tempList = []
+    for (const index in cases) {
+        let item = cases[index]
+        if (item.test.value == number) {
+            if (is_remove) {
+                return path.node.cases.splice(cases.indexOf(item), 1);
+            } else {
+                return item;
+            }
+        }
+    }
+}
+
+
+//  判断set元素是否存在
+var isSameNowVal = (_arr, val) => {
+    let i = 0
+    let indexList = []
+    _arr.forEach((e, _index) => {
+        if (e == val) {
+            i += 1
+            indexList.push(_index)
+        }
+    })
+    return {
+        bool: i >= 1,
+        indexList
+    }
+}
+
+function duplicates(arr) {
+    arr.sort();
+    let Arr = [];
+    arr.forEach((elem, index) => {
+        if (elem === arr[index + 1] && Arr.indexOf(elem) === -1) {
+            Arr.push(elem);
+        }
+    })
+    return Arr;
+}
+
+
+let allNode = []
+let UniqueSuccessorNode = []
+const UniqueNodeMerging = {
+    SwitchStatement(path) {
+        let {discriminant, cases} = path.node
+        //  特征判断
+        if (!t.isIdentifier(discriminant)) return;
+        if (discriminant.name != "Oa") return;
+        cases.forEach(caseItem => {
+            const {consequent} = caseItem
+            let Expression = consequent[consequent.length - 2]
+            //  特征判断
+            if (!t.isExpressionStatement(Expression)) return;
+            if (!t.isAssignmentExpression(Expression.expression)) return;
+            //  记录所有switch节点next
+            if (!t.isNumericLiteral(Expression.expression.right)) {
+                [Expression.expression.right.consequent.value,
+                    Expression.expression.right.alternate.value].forEach(
+                    value => allNode.push(value)
+                )
+            } else {
+                allNode.push(Expression.expression.right.value)
+                UniqueSuccessorNode.push(Expression.expression.right.value)
+            }
+        })
+        //  重复的前驱节点
+        let duplicatesNode = duplicates(allNode)
+        //  求差集得出前驱节点唯一的节点
+        let UniquePredecessorNode = allNode.filter(x => !new Set(duplicatesNode).has(x))
+        // 求交集得出拥有唯一的后继节点，且后继节点的前驱节点唯一的节点
+        let onlySingleJumpNode = UniquePredecessorNode.filter(x => new Set(UniqueSuccessorNode).has(x))
+        //  唯一节点合并
+        onlySingleJumpNode.forEach(testValue => {
+            let SuccessorIndex = 0, PredecessorIndex = 0;
+            for (let index in cases) {
+                let caseItem = cases[index]
+                const {consequent} = caseItem
+                let Expression = consequent[consequent.length - 2]
+                //  特征判断
+                if (!t.isExpressionStatement(Expression)) continue;
+                if (!t.isAssignmentExpression(Expression.expression)) continue;
+                if (!t.isNumericLiteral(Expression.expression.right)) {
+                    [Expression.expression.right.consequent.value,
+                        Expression.expression.right.alternate.value].forEach(value => {
+                        if (testValue === value) PredecessorIndex = index;
+                    })
+                } else if (testValue === Expression.expression.right.value) PredecessorIndex = index;
+                if (testValue === caseItem.test.value) SuccessorIndex = index;
+                if (SuccessorIndex && PredecessorIndex) break;
+            }
+            let Successor = cases[SuccessorIndex].consequent
+            //  移除无用的跳转 next 和 break
+            cases[PredecessorIndex].consequent.pop()
+            cases[PredecessorIndex].consequent.pop()
+            //  合并节点 后继节点 => 前置节点
+            Successor.forEach( item => {
+                cases[PredecessorIndex].consequent.push(item)
+            })
+            console.log(generator(cases[PredecessorIndex]).code)
+            //  删除后继节点
+            delete path.node.cases[SuccessorIndex]
+        })
+    }
+}
+traverse(ast, UniqueNodeMerging);
+
 
 //生成新的js code，并保存到文件中输出
 let {code} = generator(ast, opts = {jsescOption: {"minimal": true}});
